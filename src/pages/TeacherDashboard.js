20491733061cb9a19c9e50697f8ef19f075d9ab3
hemanthbonala <103206161+hemanthbonala@users.noreply.github.com>
2023-11-21 01:38:35 +0530

    Add files via upload

--- src/pages/TeacherDashboard.js
+++ src/pages/TeacherDashboard.js
@@ -6,6 +6,7 @@ import '../css/TeacherDashboard.css'
 import TeacherSideBar from '../components/TeacherSideBar';
 import { Route,Routes } from 'react-router-dom';
 import CreateCourse from '../components/CreateCourse';
+import AddCourse from '../components/AddCourse';
 
 function TeacherDashboard() {
  
@@ -14,17 +15,16 @@ function TeacherDashboard() {
       return (
     <div className='teacher-main'>
             
-    
-    
+    <div className='teacher-left'>
     <TeacherSideBar/>
+    </div>
+    <div className='teacher-right'>
     <Routes>
-    <Route path='/teacher/createCourse' element={<CreateCourse/>}/>
-
+    <Route path='/teacher/createCourse' element={<AddCourse/>}/>
     </Routes>
+    </div>
     
     
-       
-
           </div>
       );
   }
